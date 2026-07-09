b8f1e328739214f7ded2375870220b1879ae52ae
aniq392 <aniq@daum.net>
2026-07-09 13:37:19 +0900

    에러 체크

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.navigation:navigation-compose:2.8.5") // 2026년 현재 안정 버전 기준
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,5 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-
+    implementation("androidx.navigation:navigation-compose:2.8.5") // 2026년 현재 안정 버전 기준
+    implementation("androidx.compose.material:material-icons-extended:1.7.0")
 }
